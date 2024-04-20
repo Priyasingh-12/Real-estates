7badb44f505c9e8c22b65a5139b28fbadf3fe1dc
Priya Singh <105209464+Priyasingh22@users.noreply.github.com>
2024-04-20 13:59:21 +0530

    navbar and home  pages added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+ import Home from './route/home/Home';
+// import Login from './route/login/Login';
+// import Map from './route/map/Map';
+// import ProfilePage from './route/Profile/ProfilePage'
+
+// import ItemDetailPage from './route/ItemDetailPage'
+
+// import NotFounded404 from './route/NotFounded404';
+// import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/Home" element={<Home />} />
+        {/* <Route exact path="/Login" element={<Login />} /> */}
+        {/* <Route exact path="/Map" element={<Map />} /> */}
+        {/* <Route path='/profile' element={<ProfilePage />} /> */}
+
+        {/* <Route path="*" element={<NotFounded404 />} /> */}
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
   );
 }
 
 export default App;
+ 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from 'react';
+import logo from "../assets/logo.png";
+import { TfiAlignJustify } from "react-icons/tfi";
+import { Link } from "react-router-dom";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeTwoToneIcon from "@mui/icons-material/DarkModeTwoTone";
+import { TfiClose } from "react-icons/tfi";
+import { UserContext } from '../context/UserContext';
+import { useRef } from 'react';
+
+import "./navbar.css";
+
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
+  const {userInfo, setUserInfo} = React.useContext(UserContext)
+
+  const bodyRef = useRef(document.body);
+
+  const logoutUser = () => {
+    fetch(`${import.meta.env.VITE_BASE_URL}/api/v1/auth/logout`, {
+      credentials: 'include',
+      method: 'POST'
+    })
+    setUserInfo(null);
+  }
+
+  const handleDarkModeToggle = () => {
+    setDarkMode(!darkMode);
+  }
+
+  useEffect(()=>{
+    if(darkMode){
+      bodyRef.current.classList.add('dark-mode');
+      
+    } else {
+      bodyRef.current.classList.remove('dark-mode');
+    }
+  }, [darkMode])
+
+  return (
+    <header className= {`header ${darkMode ? 'dark-mode' : ''}` }>
+      <div className="header-container ">
+        <div className="navbar-sm" id="left">
+          <img
+            src={logo}
+            className="nav-logo"
+            alt="my home"
+            height="60px"
+            width="150px"
+          />
+          <button onClick={() => setToggle(!toggle)}>
+            {toggle ? <TfiClose /> : <TfiAlignJustify />}
+          </button>
+        </div>
+        <nav className={toggle ? "showNav" : "hideNav"}>
+          <ul>
+          <li><a href="/Home">Home</a></li>
+            <li><a href="/">Sell</a></li>
+            <li><a href="/">Buy</a></li>
+            <li><a href="/">Mortgage</a></li>
+            <li><a href="/">Agent</a></li>
+            <li><a href="/Map">Map</a></li>
+          </ul>
+        </nav>
+        
+        <div className="header-right">
+          <div className="dark-mode-toggle">
+            <button onClick={handleDarkModeToggle}>
+              {darkMode ? <LightModeOutlinedIcon /> : <DarkModeTwoToneIcon />}
+            </button>
+          </div>
+
+          <div className="header-right-login">
+            {userInfo ? (
+              <>
+                <Link to="/create-ad">List My Property</Link>
+                <Link to="/profile">Profile</Link>
+                <Link onClick={logoutUser}>Logout</Link>
+              </>
+            ) : (
+              <>
+                <Link to="/Login">Login</Link>
+                <Link to="/Login">Signup</Link>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
